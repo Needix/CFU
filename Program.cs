9fb6f8431dfb48a72059496092eebfd685ae7236
J. Oeldemann <needcoding@gmail.com>
2015-09-13 22:51:19 +0200

    - Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Custom_FTP_Uploader.ProjectSRC.Controller;
+using Custom_FTP_Uploader.ProjectSRC.GUI;
+
+namespace Custom_FTP_Uploader {
+    static class Program {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            GUIView view = new GUIView();
+            GUIController controller = new GUIController(view); //Controller is saved in view as reference
+            view.RegisterController(controller);
+
+            Application.Run(view);
+        }
+    }
+}
